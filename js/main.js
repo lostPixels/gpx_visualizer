47b5235e75b06ac47ee6d8f8412636030e8052b9
James Planit <Jtm@planitagency.com>
2013-09-24 18:00:58 -0400

    routing setup

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
+'use strict';
+
 function welcome($scope){
   
 }
 
 
-
-var App = angular.module('homepage', []);
-
-App.controller('welcome', function($scope, $http) {
+function welcome($scope, $http) {
   $http.get('mock_data/home.json')
        .then(function(res){
           $scope.recent_rides = res.data.recent_rides;                
         });
-});
+};
+
+function newRide($scope, $http) {
+	console.log('new ride!')
+  $http.get('mock_data/home.json')
+       .then(function(res){
+          //$scope.recent_rides = res.data.recent_rides;                
+        });
+};
+
+function rideDetail($scope, $http) {
+  $http.get('mock_data/ride.json')
+       .then(function(res){
+          $scope.ride_details = res.data;                
+        });
+};
+
+
+
+angular.module('gpxRide', []).
+  config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      when('/new', {templateUrl: 'partials/new-ride.html', controller: newRide}).
+      when('/ride/:rideId', {templateUrl: 'partials/ride-detail.html', controller: rideDetail}).
+      when('/', {templateUrl: 'partials/home.html', controller: welcome}).
+      otherwise({redirectTo: '/'});
+}]);
